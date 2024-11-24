f681ec3aa2ff2e0cbc819c44655ba5f8f3951ab2
cxxyyl <accounts@cxxyyl.xyz>
2024-11-25 00:32:08 +0100

    placement birthchart

--- site/snippets/birthchart.php
+++ site/snippets/birthchart.php
@@ -1,21 +1,21 @@
 <!--                     
-        _______________________
-        ✦ thesis.digitale.grafik chart ✦
-        ⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺  
+                                                                    _______________________
+                                                                    ✦ thesis.digitale.grafik chart ✦
+                                                                    ⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺  
 
-    ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾       ✦  ✦   
-    ☾                                       ☾    ✦ first ✦  ✦
-    ☾    Sun: Libra & in 5th house          ☾      ✦ uploaded ✦
-    ☾    Gemini rising                      ☾         ✦  ✦  ✦
-    ☾    Moon: Aries                        ☾     
-    ☾        Ruler: Mercury                 ☾
-    ☾        Lilith: Libra                  ☾      ✦   ✦   ✦   ✦   ✦           
-    ☾        Chiron: Aries                  ☾    ✦ Nürnberg, Heztner ✦
-    ☾        Mercury and Venus in Scorpio   ☾      ✦ October 16th ✦
-    ☾        Mars in Cancer                 ☾    ✦ 20:45 ✦  ✦  ✦  
-    ☾        Jupiter in Gemini              ☾      ✦  ✦   
-    ☾                                       ☾
-    ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾
+                                                                ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾       ✦  ✦   
+                                                                ☾                                       ☾    ✦ first ✦  ✦
+                                                                ☾    Sun: Libra & in 5th house          ☾      ✦ uploaded ✦
+                                                                ☾    Gemini rising                      ☾         ✦  ✦  ✦
+                                                                ☾    Moon: Aries                        ☾     
+                                                                ☾        Ruler: Mercury                 ☾
+                                                                ☾        Lilith: Libra                  ☾      ✦   ✦   ✦   ✦   ✦           
+                                                                ☾        Chiron: Aries                  ☾    ✦ Nürnberg, Heztner ✦
+                                                                ☾        Mercury and Venus in Scorpio   ☾      ✦ October 16th ✦
+                                                                ☾        Mars in Cancer                 ☾    ✦ 20:45 ✦  ✦  ✦  
+                                                                ☾        Jupiter in Gemini              ☾      ✦  ✦   
+                                                                ☾                                       ☾
+                                                                ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾ ☾
 
 
 --> 
